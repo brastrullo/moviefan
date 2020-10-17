cd13d6b53e9bbdbf7a2a4e529805000eb0ab3fca
Bradley Rastrullo <brastrullo@gmail.com>
2020-10-17 11:08:51 -0400

    feat: WIP integrate tmdb api with movie component

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,6 +1,9 @@
 <template>
   <label :for="name">{{ label }}</label>
-  <input type="text" :name="name" :id="name" />
+  <input v-model="query" type="text" :name="name" :id="name" />
+  <p>{{ query }}</p>
+  <button @click="searchMovie">Search</button>
+
   <ul>
     <li v-for="movie in moviesSelected" :key="movie.id">
       <article>
@@ -15,17 +18,30 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "SearchMovie",
+  name: "Movies",
   data() {
     return {
+      query: "",
       moviesFound: [],
-      moviesSelected: [{ id: 0, name: "movie1" }]
+      moviesSelected: [{ id: 0, name: "movie1" }],
     };
   },
   props: {
     name: String,
-    label: String
-  }
+    label: String,
+  },
+  methods: {
+    async searchMovie() {
+      const query = this.query;
+      console.log(query)
+      const API_KEY = `ea9385095138c0c18e3aca7590507b54`;
+      const BASE_URL = `https://api.themoviedb.org/3/search/movie`;
+      const FETCH_URL = `${BASE_URL}?api_key=${API_KEY}&query=${encodeURIComponent(query)}&page=1`;
+      let res = await fetch(FETCH_URL);
+      let data = await res.json();
+      console.log(data)
+    },
+  },
 });
 </script>
 
@@ -38,5 +54,8 @@ input {
   background: none;
   border: none;
   border-bottom: 1px solid #333;
+}
+button {
+  margin: 0.5em 0;
 }
 </style>
