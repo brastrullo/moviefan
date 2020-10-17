5a2bec0d50b2c38de8ddea56f8ffd77e05acc40d
Bradley Rastrullo <brastrullo@gmail.com>
2020-10-17 11:46:15 -0400

    feat: WIP movies component
    - add html structure for json in dom
    - add poster img url

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -3,11 +3,15 @@
   <input v-model="query" type="text" :name="name" :id="name" />
   <p>{{ query }}</p>
   <button @click="searchMovie">Search</button>
-  
+
   <ul>
     <li v-for="movie in moviesFound" :key="movie.id">
       <article>
-        {{ movie }}
+        <p>{{ movie.title }}</p>
+        <img :src="movie.poster" :alt="movie.title" />
+        <p>{{ movie.overview }}</p>
+        <p>{{ movie.release_date }}</p>
+        <p>{{ movie.user_score }}</p>
       </article>
       <button>x</button>
     </li>
@@ -41,22 +45,35 @@ export default defineComponent({
   methods: {
     async searchMovie() {
       const query = this.query;
-      console.log(query)
+      console.log(query);
       const API_KEY = `ea9385095138c0c18e3aca7590507b54`;
       const BASE_URL = `https://api.themoviedb.org/3/search/movie`;
-      const FETCH_URL = `${BASE_URL}?api_key=${API_KEY}&query=${encodeURIComponent(query)}&page=1`;
+      const FETCH_URL = `${BASE_URL}?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+      )}&page=1`;
+      const IMG_URL = `https://image.tmdb.org/t/p/w92`;
+      // poster sizes from config api
+      // const poster_sizes = [
+      //   "w92",
+      //   "w154",
+      //   "w185",
+      //   "w342",
+      //   "w500",
+      //   "w780",
+      //   "original",
+      // ];
       let res = await fetch(FETCH_URL);
       let data = await res.json();
-      let movies = data.results.map(obj => ({
+      let movies = data.results.map((obj) => ({
         id: obj.id,
         title: obj.title,
         overview: obj.overview,
         release_date: obj.release_date,
         user_score: obj.vote_average,
-        poster: obj.poster_path
-      }))
+        poster: `${IMG_URL}${obj.poster_path}`,
+      }));
       this.moviesFound = movies;
-      console.log(movies)
+      console.log(movies);
     },
   },
 });
